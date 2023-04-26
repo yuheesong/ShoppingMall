46e7eec344111a2cec9cd6bb40453367502b4ddc
yuheesong <thddbgml345@naver.com>
2023-04-26 14:08:21 +0900

    [회원 가입] 회원가입 성공시 메인페이지로 리다이렉트, 실패시 회원가입 페이지로 돌아가 실패이유 출력

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -25,12 +28,22 @@ public class MemberController {
         return "member/memberForm";
     }
 
+
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto){
+    public String newMember(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult, Model model){
 
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
 
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 
