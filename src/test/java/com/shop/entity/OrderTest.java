13b0807b37eb690883dafbb6c08f4f1569656cbc
yuheesong <thddbgml345@naver.com>
2023-04-28 20:54:04 +0900

    [엔티티 조회] 주문 엔티티 조회 테스트 (지연 로딩)

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -115,7 +115,9 @@ class OrderTest {
 
         OrderItem orderItem = orderItemRepository.findById(orderItemId)
                 .orElseThrow(EntityNotFoundException::new);
-        System.out.println("Order class : " +
-                orderItem.getOrder().getClass());
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
+        System.out.println("==========================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("==========================");
     }
 }
