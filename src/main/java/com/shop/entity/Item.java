42cae7174d6bba9262ba04503b34ead19b9285dd
yuheesong <thddbgml345@naver.com>
2023-04-29 13:25:23 +0900

    상품 수정 구현

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -39,6 +40,14 @@ public class Item extends BaseEntity {
     //private LocalDateTime regTime; //등록 시간, Auditing로 대체
 
     //private LocalDateTime updateTime; //수정 시간, Auditing로 대체
+
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
 
 }
