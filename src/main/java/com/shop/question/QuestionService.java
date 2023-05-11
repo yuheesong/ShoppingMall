f385820fc6db83c02e80126d860ddea4a7e33ed9
yuheesong <thddbgml345@naver.com>
2023-05-11 16:40:25 +0900

    [자유게시판] - 질문 상세

--- src/main/java/com/shop/question/QuestionService.java
+++ src/main/java/com/shop/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.shop.question;
 
+import com.shop.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,13 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
