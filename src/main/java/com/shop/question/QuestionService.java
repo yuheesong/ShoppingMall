54afa432f24ca596025a6df00c64911a611e3f6c
yuheesong <thddbgml345@naver.com>
2023-05-11 17:53:27 +0900

    [자유게시판] - 페이징 기능 구현

--- src/main/java/com/shop/question/QuestionService.java
+++ src/main/java/com/shop/question/QuestionService.java
@@ -2,6 +2,9 @@ package com.shop.question;
 
 import com.shop.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,8 +16,9 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
