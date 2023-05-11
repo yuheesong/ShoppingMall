14ecb926e47eeda49687c7f9a153e35c39f36668
yuheesong <thddbgml345@naver.com>
2023-05-11 20:01:04 +0900

    상품관리 화면 수정

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -95,7 +95,7 @@ public class ItemController {
     @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
     public String itemManage(ItemSearchDto itemSearchDto,
                              @PathVariable("page") Optional<Integer> page, Model model){
-        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0,3);
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0,10);
         Page<Item> items =
                 itemService.getAdminItemPage(itemSearchDto, pageable);
         model.addAttribute("items", items);
