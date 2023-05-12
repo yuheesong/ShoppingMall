7662ced9e0ea9a53f331a514e6b40bcc574d18d6
yuheesong <thddbgml345@naver.com>
2023-05-12 21:02:34 +0900

    답변 수정과 삭제

--- src/main/java/com/shop/answer/AnswerService.java
+++ src/main/java/com/shop/answer/AnswerService.java
@@ -1,10 +1,12 @@
 package com.shop.answer;
 
+import com.shop.DataNotFoundException;
 import com.shop.question.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +21,23 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer){
+        this.answerRepository.delete(answer);
     }
 }
